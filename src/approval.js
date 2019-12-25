a7f979a158f2d49f81f733fd338c964b84f7c90b
minhajspace <minhaj.space@gmail.com>
2019-12-25 21:13:57 +0530

    changes

--- src/approval.js
+++ src/approval.js
@@ -9,7 +9,7 @@ const ApprovalCard = props => {
         <div className="content">{props.children}</div>
         <div className="extra content">
           <div className="ui two buttons">
-            <div className="ui basic green button">Approve</div>
+            <div className="ui basic yellow button">Approve</div>
             <div className="ui basic red button">Decline</div>
           </div>
         </div>
